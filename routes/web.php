6c0c6b958ebbd4706a24275f7ef1b2a74892b7ef
rizka30 <2041720095@student.polinema.ac.id>
2022-02-23 21:29:51 +0700

    Praktikum3 JS2

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +16,8 @@ use App\Http\Controllers\ArticleController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
+
+/* pRAKTIKUM 1
 Route::get('/', function () {
   echo "Hi! Selamat Datang di Website Laravel";
 });
@@ -34,7 +36,8 @@ Route::get('/article/{id}', function ($id= '2') {
     echo ("Ini adalah Halaman Artikel dengan ID ".$id);
 });
 */
-/*
+
+/* PRAKTIKUM 2 MODIF 1
 Route::get('/','App\Http\Controllers\PageController@index');
 
 Route::get('/about','App\Http\Controllers\PageController@about');
@@ -42,6 +45,56 @@ Route::get('/about','App\Http\Controllers\PageController@about');
 Route::get('/articles/{id}','App\Http\Controllers\PageController@articles');
 */
 
+/*PRAKTIKUM 2 MODIF 2
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+*/
+
+// PRAKTIKUM 3
+
+Route::get('/', function () {
+    echo "Hallo! Selamat Datang";
+});
+
+Route::prefix('category') -> group(function () {
+    Route::get("/marbel-edu-games", function () {
+        echo "Halaman Marbel Edu Games";
+    });
+    Route::get("/marbel-and-friends-kids-games", function () {
+        echo "Halaman Marbel and Friends Kids Games";
+    });
+    Route::get("/riri-story-books", function () {
+        echo "Halaman Riri Stroy Books";
+    });
+    Route::get("/kolak-kids-songs", function () {
+        echo "Halaman Kolak Kids Songs";
+    });
+});
+
+Route::prefix('news') -> group(function () {
+    Route::get("", function () {
+        echo "Ini Merupakan Halaman NEWS";
+    });
+    Route::get("/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19", function () {
+        echo "Ini Merupakan Halaman Studio Berbagi Untuk Warga Sekitar Terdampak Covid-19";
+    });
+});
+
+Route::prefix('program') -> group(function () {
+    Route::get("/karir" , function () {
+        echo "Ini adalah Halaman Karir";
+    });
+    Route::get("/magang" , function () {
+        echo "Ini adalah Halaman Magang";
+    });
+    Route::get("/kunjungan-industri" , function () {
+        echo "Ini adalah Halaman Kunjungan Industri";
+    });
+});
+
+Route::get("/about-us" , function () {
+        echo "Nama  : Rizka Musyarofatul Hidayah <br>";
+        echo "NIM   : 2041720095 <br>";
+        echo "Kelas : TI-2G";
+});

6f5dcbada16a88ca6ef95a8dfba7deee12993a43
rizka30 <2041720095@student.polinema.ac.id>
2022-03-23 16:22:10 +0700

    Praktikum2 JS6 Login Admin

--- routes/web.php
+++ routes/web.php
@@ -111,3 +111,7 @@ Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

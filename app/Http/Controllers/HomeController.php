bcc87b2e9dd2453d705217161e550fec9b904c65
rizka30 <2041720095@student.polinema.ac.id>
2022-03-23 21:22:33 +0700

    Praktikum3 JS 6 Register dan CurrentUser

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
-        $posts=Post::all();
-        return view('home', ['posts'=>$posts]);
+        // $posts=Post::all();
+        // return view('home', ['posts'=>$posts]);
 
+        $user = Auth::user();
+        return view('home', ['user' => $user]);
     }
 }
